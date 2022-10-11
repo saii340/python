9d5b508ba8479740656013baac115d565b51f3e2
saii340 <111050937+saii340@users.noreply.github.com>
2022-10-11 16:17:15 +0530

    Add files via upload
    python 1

---
+++ main.py
@@ -0,0 +1,49 @@
+my_list = [1, 2, 2, 4, 4, 5, 6, 8, 10, 13, 22, 35, 52, 83]
+list=[]
+for i in my_list:
+    if i>=10:
+       list.append(i)
+print(list)
+n=int(input())
+list=[]
+for i in my_list:
+    if i>n:
+        list.append(i)
+print(list)
+
+employees = [{
+  "name": "Tina",
+  "age": 30,
+  "birthday": "1990-03-10",
+  "job": "DevOps Engineer",
+  "address": {
+    "city": "New York",
+    "country": "USA"
+  }
+},
+{
+  "name": "Tim",
+  "age": 35,
+  "birthday": "1985-02-21",
+  "job": "Developer",
+  "address": {
+    "city": "Sydney",
+    "country": "Australia"
+  }
+}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
