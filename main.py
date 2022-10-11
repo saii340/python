8b039b9f329119fb5a396dbecc501f0c0b0ec374
saii340 <111050937+saii340@users.noreply.github.com>
2022-10-11 16:18:09 +0530

    Update main.py
    python

--- main.py
+++ main.py
@@ -11,27 +11,6 @@ for i in my_list:
         list.append(i)
 print(list)
 
-employees = [{
-  "name": "Tina",
-  "age": 30,
-  "birthday": "1990-03-10",
-  "job": "DevOps Engineer",
-  "address": {
-    "city": "New York",
-    "country": "USA"
-  }
-},
-{
-  "name": "Tim",
-  "age": 35,
-  "birthday": "1985-02-21",
-  "job": "Developer",
-  "address": {
-    "city": "Sydney",
-    "country": "Australia"
-  }
-}]
-
 
 
 
