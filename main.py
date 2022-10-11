aacf76f9fc92706bd94cf93b8dae3c5a65f6e74c
saii340 <111050937+saii340@users.noreply.github.com>
2022-10-11 16:44:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ for i in my_list:
     if i>n:
         list.append(i)
 print(list)
+####output###
+[10, 13, 22, 35, 52, 83]
+4
+[5, 6, 8, 10, 13, 22, 35, 52, 83]
+
+
+employee = {
+  "name": "Tim",
+  "age": 30,
+  "birthday": "1990-03-10",
+  "job": "DevOps Engineer"
+}
+employee['job']='software engineer'
+del employee['age']
+for i in employee:
+ print(i, ':' ,employee[i])
+###output####
+name : Tim
+birthday : 1990-03-10
+job : software engineer
+
 
 
 
